9be3874d5293d3f084765e8a09e3c8d6767164c3
bhargavvangara <bhargavvangara5@gmail.com>
2025-05-05 22:37:43 +0530

    initial commit

---
+++ src/main/java/com/jpademo/InheritanceDemoUsingPayment/InheritanceDemoUsingPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.jpademo.InheritanceDemoUsingPayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InheritanceDemoUsingPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InheritanceDemoUsingPaymentApplication.class, args);
+	}
+
+}
